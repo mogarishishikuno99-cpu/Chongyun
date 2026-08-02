871ee1bed4a167a9a26568667c3008f241d4c591
taijylangu30-lgtm <taijylangu30@gmail.com>
2026-08-02 15:47:55 +0100

    Bump version to 1.6 and update response messages

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -4,7 +4,7 @@ module.exports = {
 	config: {
 		name: "onlyadminbox",
 		aliases: ["onlyadbox", "adboxonly", "adminboxonly"],
-		version: "1.5",
+		version: "1.6",
 		author: "NTKhang x Hori",
 		countDown: 5,
 		role: 1,
@@ -22,18 +22,18 @@ module.exports = {
 	},
 	langs: {
 		fr: {
-			turnedOn: "C'est fait ! Maintenant, je n'écoute plus que les admins de ce groupe. Les autres, pas bouger !",
-			turnedOff: "Libre accès ! Tout le monde peut recommencer à me parler ici... Ne me faites pas regretter mon choix !",
-			turnedOnNoti: "Alerte activée ! Je vais afficher un message si un membre lambda essaie de me donner des ordres.",
-			turnedOffNoti: "Silence radio. S'ils essaient de m'utiliser, je vais juste les ignorer en douce.",
-			syntaxError: "Raaah, tu t'es trompé(e) ! Écris correctement '{pn} on' ou '{pn} off'. C'est pourtant pas si compliqué !"
+			turnedOn: "✅ | C'est fait ! Maintenant, je n'écoute plus que les admins de ce groupe. Les autres, pas bouger !",
+			turnedOff: "✅ | Libre accès ! Tout le monde peut recommencer à me parler ici... Ne me faites pas regretter mon choix !",
+			turnedOnNoti: "✅ | Alerte activée ! Je vais afficher un message si un membre lambda essaie de me donner des ordres.",
+			turnedOffNoti: "✅ | Silence radio. S'ils essaient de m'utiliser, je vais juste les ignorer en douce.",
+			syntaxError: "❌ | Raaah, tu t'es trompé(e) ! Écris correctement '{pn} on' ou '{pn} off'. C'est pourtant pas si compliqué !"
 		},
 		en: {
-			turnedOn: "Done! Now I only listen to the group admins. Everyone else, back off!",
-			turnedOff: "Open access! Everyone can talk to me again... Don't make me regret this!",
-			turnedOnNoti: "Alert on! I'll call out anyone who isn't an admin trying to boss me around.",
-			turnedOffNoti: "Silence. If they try to use me, I'll just ignore them quietly.",
-			syntaxError: "Ugh, you messed up! Use '{pn} on' or '{pn} off'. It's not that hard!"
+			turnedOn: "✅ | Done! Now I only listen to the group admins. Everyone else, back off!",
+			turnedOff: "✅ | Open access! Everyone can talk to me again... Don't make me regret this!",
+			turnedOnNoti: "✅ | Alert on! I'll call out anyone who isn't an admin trying to boss me around.",
+			turnedOffNoti: "✅ | Silence. If they try to use me, I'll just ignore them quietly.",
+			syntaxError: "❌ | Ugh, you messed up! Use '{pn} on' or '{pn} off'. It's not that hard!"
 		}
 	},
 	onStart: async function ({ args, message, event, threadsData, getLang }) {
